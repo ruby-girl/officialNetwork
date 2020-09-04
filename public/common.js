c2a555cdb001a27ea8b8ab954650150f786c8279
ruby-girl <597032797@qq.com>
2020-09-04 15:03:03 +0800

    初始化

---
+++ public/common.js
@@ -0,0 +1,21 @@
+document.write('<!-----------  公共js和css start----------->');
+document.write('' +
+    '<script src="./js/jquery-1.11.0.min.js"></script>\n' +
+    '<script src="./js/jquery.SuperSlide.2.1.1.js" type="text/javascript"></script>\n' +
+    '<script src="./js/bootstrap.min.js"></script>\n' +
+    '<script src="./js/prefixfree.min.js"></script>\n' +
+    '<script src="./js/bootsnav.js"></script>\n' +
+    '<script src="./plugin/swiper3/js/swiper.min.js"></script>\n' +
+    '<script src="./plugin/swiper3/js/swiper.animate1.0.2.min.js"></script>\n' +
+	'<script src="./js/public.js"></script>\n' +
+	'<script src="js/wow.min.js"></script>\n' +
+	'<script src="./js/vendors.js"></script>\n' +
+	'<script src="./js/jquery.waypoints.min.js"></script>\n' +
+	'<script src="./js/jquery.countup.js"></script>\n' + 
+	'<link href="./css/bootstrap.min.css" rel="stylesheet">\n' + 
+	'<link href="./fonts/FontAwesome/font-awesome.css" rel="stylesheet">\n' +
+	'<link href="./css/animate.css" rel="stylesheet">\n' +
+	'<link href="./css/style.css" rel="stylesheet">\n' +
+    '');
+document.write('<!-----------  公共js和css end----------->');
+
